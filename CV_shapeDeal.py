1f0d3784bb67360accea2cbe512b9424e182a3d6
CSUaltitue <703362086@qq.com>
2018-10-13 00:03:51 +0800

    shpe deal 02

--- CV_shapeDeal.py
+++ CV_shapeDeal.py
@@ -39,7 +39,8 @@ m = img01.shape[0]
 n = img01.shape[1]
 print(img01.shape)
 
-#revert gred 
+#revert gray 
+#cv2 has it's owne func :bitwise_not 
 '''
 for i in range(m):
     for j in range(n):
@@ -48,32 +49,53 @@ for i in range(m):
 
 cv2.imwrite("imgrevert.jpg",img01)
 '''
+'''
 #add noise 
+
 for i in range(8000):
     x=np.random.randint(0,m)
     y=np.random.randint(0,n)
     img01[x][y]=255 ###255 means bright best
-    
-cv2.imwrite("imgaddnoise.jpg",img01)
+'''
 
-#fushi
+cv2.imwrite("imgaddnoise.jpg",img01)
+# fushi & pengzhang all for white color (gred ==255)
+
+#fushi decress noise 
 erodepic = cv2.erode(img01,kernel)
-cv2.imwrite("erodepic.jpg",erodepic)
-#pengzhang 
-dilatepic = cv2.dilate(erodepic,kernel)
+cv2.imwrite("erodepic.jpg",img01)
+
+#pengzhang incress noise 
+dilatepic = cv2.dilate(img01,kernel)
 cv2.imwrite("dilatepic.jpg",dilatepic)
 
-#close 
+
+#close  pengzhang + fushi  incress white noise
 closepic = cv2.morphologyEx(img01,cv2.MORPH_CLOSE,kernel)
 
 
-#open
+#open= fushi+pengzhang  decress white noise 
+
 openpic = cv2.morphologyEx(img01,cv2.MORPH_OPEN,kernel)
 
 cv2.imwrite("closepic.jpg",closepic)
 cv2.imwrite("openpic.jpg",openpic)
 
 
+#find edge
+#将两幅图像相减获得边，第一个参数是膨胀后的图像，第二个参数是腐蚀后的图像
+edge = cv2.absdiff(dilatepic,erodepic)
 
+cv2.imwrite("edge.jpg",edge)
+#binary pic
+#上面得到的结果是灰度图，将其二值化以便更清楚的观察结果
+retval, edge = cv2.threshold(edge, 40, 255, cv2.THRESH_BINARY)
+
+cv2.imwrite("edge01.jpg",edge)
+
+#反色，即对二值图每个像素取反
+edge02 = cv2.bitwise_not(edge)
+
+cv2.imwrite("edge02.jpg",edge02)
 
 
